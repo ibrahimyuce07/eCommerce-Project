4a16508c1e96bebe17f39a4c1f4bb141f1d1d9ed
İbrahim Yüce <ibrahimyucee@outlook.com>
2020-12-22 23:15:17 +0300

    spring boot backend

---
+++ 02-backend/springboot-ecommerce/src/main/java/com/ibrahim/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,12 @@
+package com.ibrahim.ecommerce.dao;
+
+import com.ibrahim.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
+
+// collectionResourceRel: name of JSON entry
+//path: /product-category
---
+++ 02-backend/springboot-ecommerce/src/main/java/com/ibrahim/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.ibrahim.ecommerce.dao;
+
+import com.ibrahim.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> { //entity, primary key
+
+}
